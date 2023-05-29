d0f81521b29a278003d5c89439b5af545deb7cc5
Highdrien <adrien.zabban@outlook.fr>
2023-05-29 18:30:15 +0200

    implement config on test

--- src/test.py
+++ src/test.py
@@ -1,20 +1,24 @@
 from tqdm import tqdm
+import numpy as np
 
 import torch
 import torch.nn as nn
 
 from src.model import get_model
 from src.dataloader import creates_generators
+from src.checkpoints import get_checkpoint_path
 
+from config.utils import test_logger
 
-def test(config):
+def test(logging_path, config):
 
     _, _, test_loader = creates_generators(config)
 
     # Instancier le modèle
-    model = get_model()
+    model = get_model(config)
 
     # Load model's weight
+    checkpoint_path = get_checkpoint_path(config, logging_path)
     model.load_state_dict(torch.load(checkpoint_path))
 
     # Définir la loss
@@ -22,24 +26,22 @@ def test(config):
 
     # Évaluation finale sur l'ensemble de test
     model.eval()
-    test_loss = 0.0
+    test_loss = []
 
     with torch.no_grad():
         for inputs, targets in tqdm(test_loader):
             user_ids = inputs[:, 0]
             item_ids = inputs[:, 1]
+            
+            if user_ids.shape[0] == config.test.batch_size:
 
-            outputs = model(user_ids, item_ids)
-            loss = criterion(outputs.squeeze(), targets)
+                outputs = model(user_ids, item_ids)
+                loss = criterion(outputs.squeeze(), targets)
 
-            test_loss += loss.item() * inputs.size(0)
+                test_loss.append(loss.item())
+            
 
-    test_loss /= len(test_loader)
+    test_loss = np.mean(test_loss)
 
-    print(f"Test Loss: {test_loss:.4f}")
-
-if __name__ == '__main__':
-    checkpoint_path = 'model.pth'
-    test(checkpoint_path)
-
+    test_logger(logging_path, ['MSE'], [test_loss])
 
