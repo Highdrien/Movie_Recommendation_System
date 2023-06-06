01414575b3e3262857356a9ebc2225df545d8861
Highdrien <adrien.zabban@outlook.fr>
2023-06-06 18:51:52 +0200

    add validation

--- src/train.py
+++ src/train.py
@@ -1,14 +1,12 @@
-import numpy as np
-
 from tqdm import tqdm
 
 import torch
-import torch.nn as nn
 import torch.optim as optim
 
 from src.model import get_model
-from src.utils import save_learning_curves
+from src.loss import MaskedMSELoss
 from src.dataloader import get_data
+from src.utils import save_learning_curves
 from src.checkpoints import save_checkpoint_all, save_checkpoint_best, save_checkpoint_last
 
 from config.utils import train_logger, train_step_logger
@@ -16,74 +14,72 @@ from config.utils import train_logger, train_step_logger
 
 def train(config):
 
+    # get data
     user_ids, item_ids, edge_index, target = get_data(config)
 
-    # Instancier le modèle
+    # Split target for train and validation
+    num_users = target.shape[0]
+    split_1 = int(num_users * config.data.split_1)
+    split_2 = int(num_users * config.data.split_2) + split_1
+    train_target = target[:split_1]
+    val_target = target[split_1:split_2]
+
+    del target
+
     model = get_model(config)
 
     # Définir la fonction de perte et l'optimiseur
-    criterion = torch.nn.MSELoss()
+    criterion = MaskedMSELoss()
+    # criterion = AdvancedMaskedMSELoss()
     optimizer = optim.Adam(model.parameters(), lr=config.model.learning_rate)
 
-    # logging_path = train_logger(config)
+    logging_path = train_logger(config)
     best_epoch, best_val_loss = 0, 10e6
 
-    ###############################################################
-    # Start Training                                              #
-    ###############################################################
-    for epoch in range(1, config.train.epochs + 1):
+    train_loss_list = []
+    val_loss_list = []
+
+    epochs_range = tqdm(list(range(1, config.train.epochs + 1)))
+    for epoch in epochs_range:
+
+        # Training
         model.train()
-        train_loss = []
-        
-        output = model(user_ids, item_ids, edge_index)
-        loss = criterion(output.flatten(), target.flatten().float())
-        print(loss.item())
-        train_loss.append(loss.item())
+        train_predict = model(user_ids, item_ids, edge_index)[:split_1]
+        loss = criterion(target=train_target, predict=train_predict)
+        train_loss = loss.item()
+        train_loss_list.append(train_loss)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
-        ###############################################################
-        # Start Validation                                            #
-        ###############################################################
-        # model.eval()
-        # val_loss = []
-        # val_range = tqdm(val_loader)
+        # Validation
+        with torch.no_grad():
+            model.eval()
+            val_predict = model(user_ids, item_ids, edge_index)[split_1:split_2]
+            loss = criterion(target=val_target, predict=val_predict)
+            val_loss = loss.item()
+            val_loss_list.append(val_loss)
 
-        # with torch.no_grad():
-        #     for inputs, targets in val_range:
-        #         user_ids = inputs[:, 0]
-        #         item_ids = inputs[:, 1]
-                
-        #         outputs = model(user_ids, item_ids)
-        #         loss = criterion(outputs.squeeze(), targets)
-                
-        #         val_loss.append(loss.item())
+        epochs_range.set_description("epoch: %4d || loss: %4.4f || val_loss: %4.4f" % (epoch, train_loss, val_loss))
+        epochs_range.refresh()
 
-        #         val_range.set_description("VAL   -> epoch: %4d || val_loss: %4.4f" % (epoch, np.mean(val_loss)))
-        #         val_range.refresh()
+        # Save Scores in logs
+        train_step_logger(logging_path, epoch, train_loss, val_loss, [], [])
 
-        # val_loss = np.mean(val_loss)
+        # Save model according the configuration
+        if config.model.save_checkpoint == 'all':
+            save_checkpoint_all(model, logging_path, epoch)
 
-        ###################################################################
-        # Save Scores in logs                                             #
-        ###################################################################
-
-    #     train_step_logger(logging_path, epoch, train_loss, val_loss, [], [])
-
-    #     if config.model.save_checkpoint == 'all':
-    #         save_checkpoint_all(model, logging_path, epoch)
-
-    #     elif config.model.save_checkpoint == 'best':
-    #         best_epoch, best_val_loss = save_checkpoint_best(model, logging_path, epoch, best_epoch, val_loss, best_val_loss)
+        elif config.model.save_checkpoint == 'best':
+            best_epoch, best_val_loss = save_checkpoint_best(model, logging_path, epoch, best_epoch, val_loss, best_val_loss)
 
     
-    # if config.model.save_checkpoint == 'best':
-    #     save_checkpoint_best(model, logging_path, epoch, best_epoch, val_loss, best_val_loss, end_training=True)
+    if config.model.save_checkpoint == 'best':
+        save_checkpoint_best(model, logging_path, epoch, best_epoch, val_loss, best_val_loss, end_training=True)
 
-    # elif config.model.save_checkpoint == 'last':
-    #     save_checkpoint_last(config, model, logging_path)
+    elif config.model.save_checkpoint == 'last':
+        save_checkpoint_last(config, model, logging_path)
 
-    # if config.train.save_learning_curves:
-    #     save_learning_curves(logging_path)
+    if config.train.save_learning_curves:
+        save_learning_curves(logging_path)
     
