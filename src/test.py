fc9defed41549018554e3f48d88cb27cb4b4a92f
Highdrien <adrien.zabban@outlook.fr>
2023-06-08 14:18:06 +0200

    add test

--- src/test.py
+++ src/test.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import torch
 
 from src.model import get_model
@@ -9,33 +7,29 @@ from src.checkpoints import get_checkpoint_path
 
 from config.utils import test_logger
 
+
 def test(logging_path, config):
 
-    # get data
-    user_ids, item_ids, edge_index, target = get_data(config)
+    # Get data
+    test_data = get_data(config, 'test')
+    test_num_users = test_data.get_num_user()
+    test_ids, test_edge_index = test_data.get_input()
+    test_target = test_data.get_target()
 
-    # Split target for train and validation
-    num_users = target.shape[0]
-    split_2 = int(num_users * config.data.split_2) + int(num_users * config.data.split_1)
-    test_target = target[split_2:]
-
-    del target
-
+    # Get model and load model's weight
     model = get_model(config)
-
-    # Load model's weight
     checkpoint_path = get_checkpoint_path(config, logging_path)
     model.load_state_dict(torch.load(checkpoint_path))
 
-    # Définir la loss
+    # Loss
     criterion = MaskedMSELoss()
 
-    # Évaluation finale sur l'ensemble de test
+    # Final evaluation on the test's dataset
     model.eval()
 
     with torch.no_grad():
         model.eval()
-        test_predict = model(user_ids, item_ids, edge_index)[split_2:]
+        test_predict = model(test_ids, test_edge_index, test_num_users)
         loss = criterion(target=test_target, predict=test_predict)
         test_loss = loss.item()
 
